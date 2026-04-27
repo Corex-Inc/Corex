a894b19ecd0f4ab87d2ea95e983d54426f5cd721
Nybik_YT <d.nasadyuk123@gmail.com>
2026-04-27 22:14:04 +0300

    Added doc

--- corex/src/main/java/dev/corexinc/corex/environment/commands/core/RepeatCommand.java
+++ corex/src/main/java/dev/corexinc/corex/environment/commands/core/RepeatCommand.java
@@ -9,6 +9,61 @@ import dev.corexinc.corex.environment.tags.core.ElementTag;
 import org.jspecify.annotations.NonNull;
 import java.util.List;
 
+/* @doc command
+ *
+ * @Name Repeat
+ * @Syntax repeat [<amount>|break|continue] (from:<#>) (as:<name>): [<commands>]
+ * @RequiredArgs 1
+ * @MaxArgs 3
+ * @ShortDescription Runs a series of braced commands several times.
+ *
+ * @Implements Repeat
+ *
+ * @Description
+ * Loops through a series of braced commands a specified number of times.
+ * To get the number of loops so far, you can use <[loopIndex]>.
+ *
+ * Optionally, specify "as:<name>" to change the definition name to something other than "loopIndex".
+ *
+ * Optionally, to specify a starting index, use "from:<#>". Note that the "amount" input is how many loops will happen, not an end index.
+ * The default "from" index is "1". Note that the value you give to "from" will be the loopIndex of the first loop.
+ *
+ * To break a repeat loop, do - repeat break
+ *
+ * To jump immediately to the next number in the loop, do - repeat continue
+ *
+ * @Tags
+ * <[loopIndex]> - Returns the number of loops so far
+ *
+ * @Usage
+ * // Use to loop through a command five times.
+ * - repeat 5:
+ *     - narrate "Narrated Number <[loopIndex]>"
+ *
+ * @Usage
+ * // Use to announce the numbers: 1, 2, 3, 4, 5.
+ * - repeat 5 as:number:
+ *     - narrate "I can count! <[number]>"
+ *
+ * @Usage
+ * // Use to narrate the numbers: 21, 22, 23, 24, 25.
+ * - repeat 5 from:21:
+ *     - narrate "Narrated Number <[loopIndex]>"
+ *
+ * @Usage
+ * // Use to stop loop if number equals 11.
+ * - repeat 100:
+ *     - if <[loopIndex]> == 11:
+ *         - repeat break // Breaks a repeat loop
+ *
+ * @Usage
+ * // Use to skip iteration.
+ * - repeat 10:
+ *     - if <[loopIndex]> == 4:
+ *         - narrate "<&c>Skipped <[loopIndex]>!"
+ *         - repeat continue // We jump to the next iteration
+ *     - narrate "The number is <[loopIndex]>!"
+ */
 public class RepeatCommand implements AbstractCommand {
 
     @Override public @NonNull String getName() {
