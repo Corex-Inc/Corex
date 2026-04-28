b79aca0555e837a5da81d2559665c50997dda672
Un1k <uncorntraids@gmail.com>
2026-04-28 22:38:38 +0500

    Test Damage Command added

---
+++ corex/src/main/java/dev/corexinc/corex/environment/commands/entity/DamageCommand.java
@@ -0,0 +1,32 @@
+package dev.corexinc.corex.environment.commands.entity;
+
+import dev.corexinc.corex.api.commands.AbstractCommand;
+import dev.corexinc.corex.engine.compiler.Instruction;
+import dev.corexinc.corex.engine.queue.ScriptQueue;
+import org.jetbrains.annotations.NotNull;
+
+public class DamageCommand implements AbstractCommand {
+    @Override
+    public @NotNull String getName() {
+        return "damage";
+    }
+
+    @Override
+    public void run(@NotNull ScriptQueue queue, @NotNull Instruction instruction) {
+    }
+
+    @Override
+    public @NotNull String getSyntax() {
+        return "1312321313213";
+    }
+
+    @Override
+    public int getMinArgs() {
+        return 1;
+    }
+
+    @Override
+    public int getMaxArgs() {
+        return 4;
+    }
+}
