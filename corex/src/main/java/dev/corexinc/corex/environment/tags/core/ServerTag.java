0c96e67df5c47185ac846320bd8d5ff73cf85632
tizis0 <hexgames84@gmail.com>
2026-05-05 23:42:08 +0600

    ServerTag: Internal JVM mechanisms

--- corex/src/main/java/dev/corexinc/corex/environment/tags/core/ServerTag.java
+++ corex/src/main/java/dev/corexinc/corex/environment/tags/core/ServerTag.java
@@ -2,8 +2,10 @@ package dev.corexinc.corex.environment.tags.core;
 
 import dev.corexinc.corex.Corex;
 import dev.corexinc.corex.api.processors.BaseTagProcessor;
+import dev.corexinc.corex.api.processors.MechanismProcessor;
 import dev.corexinc.corex.api.processors.TagProcessor;
 import dev.corexinc.corex.api.tags.AbstractTag;
+import dev.corexinc.corex.api.tags.Adjustable;
 import dev.corexinc.corex.api.tags.Attribute;
 import dev.corexinc.corex.api.tags.Flaggable;
 import dev.corexinc.corex.engine.flags.trackers.SqlFlagTracker;
@@ -61,10 +63,11 @@ import java.io.File;
  * // Get average TPS across all active regions.
  * - narrate "Average TPS: <server.regions.parse[tps].average>"
  */
-public class ServerTag implements AbstractTag, Flaggable {
+public class ServerTag implements AbstractTag, Flaggable, Adjustable {
 
     private static final String PREFIX = "server";
-    public static final TagProcessor<ServerTag> PROCESSOR = new TagProcessor<>();
+    public static final TagProcessor<ServerTag> TAG_PROCESSOR = new TagProcessor<>();
+    public static final MechanismProcessor<ServerTag> MECHANISM_PROCESSOR = new MechanismProcessor<>();
 
     private static final ServerTag INSTANCE = new ServerTag();
 
@@ -77,7 +80,7 @@ public class ServerTag implements AbstractTag, Flaggable {
         File dbFile = new File(Corex.getInstance().getDataFolder(), "serverFlags.db");
         serverTracker = new SqlFlagTracker(dbFile, "serverGlobal");
 
-        PROCESSOR.registerTag(ElementTag.class, "ram", (attribute, serverTag) -> new ElementTag(Runtime.getRuntime().maxMemory()));
+        TAG_PROCESSOR.registerTag(ElementTag.class, "ram", (attribute, serverTag) -> new ElementTag(Runtime.getRuntime().maxMemory()));
 
         /* @doc tag
          *
@@ -94,7 +97,7 @@ public class ServerTag implements AbstractTag, Flaggable {
          * // Calculate the average TPS across all active threads
          * - narrate "Average Server TPS: <server.regions.parse[tps].average>"
          */
-        PROCESSOR.registerTag(ListTag.class, "regions", (attr, obj) -> {
+        TAG_PROCESSOR.registerTag(ListTag.class, "regions", (attr, obj) -> {
             ListTag list = new ListTag();
             list.addObject(new RegionTag("global"));
 
@@ -125,7 +128,7 @@ public class ServerTag implements AbstractTag, Flaggable {
          * // List all registered structure keys
          * - narrate <server.loadedStructures.parse[key]>
          */
-        PROCESSOR.registerTag(ListTag.class, "loadedStructures", (attr, obj) -> {
+        TAG_PROCESSOR.registerTag(ListTag.class, "loadedStructures", (attr, obj) -> {
             ListTag list = new ListTag();
             for (var entry : Bukkit.getStructureManager().getStructures().entrySet()) {
                 list.addObject(new StructureTag(entry.getValue(), entry.getKey()));
@@ -151,7 +154,7 @@ public class ServerTag implements AbstractTag, Flaggable {
          * - if <server.structureTypes.contains[myplugin:my_dungeon]>:
          *   - narrate "Dungeon structure type is registered!"
          */
-        PROCESSOR.registerTag(ListTag.class, "structureTypes", (attr, obj) -> {
+        TAG_PROCESSOR.registerTag(ListTag.class, "structureTypes", (attr, obj) -> {
             ListTag list = new ListTag();
             for (StructureType type : Registry.STRUCTURE_TYPE) {
                 list.addObject(new ElementTag(type.getKey().toString()));
@@ -173,7 +176,7 @@ public class ServerTag implements AbstractTag, Flaggable {
          * // Sends a message to all online players on the server
          * - narrate targets:<server.onlinePlayers> "Hello <player.name>!" per_player
          */
-        PROCESSOR.registerTag(ListTag.class, "onlinePlayers", (attr, obj) -> {
+        TAG_PROCESSOR.registerTag(ListTag.class, "onlinePlayers", (attr, obj) -> {
             ListTag listTag = new ListTag();
             for (Player player : Bukkit.getOnlinePlayers()) {
                 listTag.addObject(new PlayerTag(player));
@@ -203,7 +206,7 @@ public class ServerTag implements AbstractTag, Flaggable {
          * - foreach <server.availableBiomes> as:biome:
          *   - narrate "Biome name: <[biome].name>"
          */
-        PROCESSOR.registerTag(ListTag.class, "availableBiomes", (attr, obj) -> {
+        TAG_PROCESSOR.registerTag(ListTag.class, "availableBiomes", (attr, obj) -> {
             BiomeAdapter adapter = NMSHandler.get().get(BiomeAdapter.class);
             ListTag list = new ListTag();
 
@@ -213,6 +216,107 @@ public class ServerTag implements AbstractTag, Flaggable {
                 list.addObject(new BiomeTag(world.getName() + "," + key.toString()));
             }
             return list;
+        });
+
+        /* @doc mechanism
+         *
+         * @Name forceGarbageCollection
+         * @Object ServerTag
+         * @Description
+         * Requests the Java Virtual Machine to run the Garbage Collector.
+         *
+         * @Warning
+         * DO NOT USE THIS UNLESS YOU KNOW EXACTLY WHAT YOU ARE DOING!
+         * IT IS HIGHLY DISCOURAGED TO EVER TOUCH THIS.
+         * Manual garbage collection calls can cause severe lag spikes ("stop-the-world" pauses)
+         * and interfere with Java's highly optimized automated memory management.
+         * Usually, if you feel you need this, you have a memory leak that needs fixing instead.
+         *
+         * @Usage
+         * // Forces a garbage collection cycle. (NOT RECOMMENDED)
+         * - adjust <server> forceGarbageCollection
+         */
+        MECHANISM_PROCESSOR.registerMechanism("forceGarbageCollection", (obj, val) -> {
+            System.gc();
+            return obj;
+        });
+
+        /* @doc mechanism
+         *
+         * @Name haltJVM
+         * @Object ServerTag
+         * @Input ElementTag(Number)
+         * @Description
+         * Instantly terminates the Java Virtual Machine with the given exit code.
+         *
+         * @Warning
+         * EXTREME DANGER! DATA CORRUPTION IMMINENT!
+         * This bypasses Bukkit.shutdown() entirely. Worlds will NOT be saved, player data
+         * will NOT be synchronized, and database connections will be abruptly severed.
+         * Using this will almost certainly result in corrupted chunks or data loss.
+         *
+         * @Usage
+         * // Instantly kills the server process with exit code 0
+         * - adjust <server> haltJVM:0
+         */
+        MECHANISM_PROCESSOR.registerMechanism("haltJVM", (obj, val) -> {
+            int exitCode = 0;
+            if (val instanceof ElementTag el && el.isDouble()) {
+                exitCode = (int) el.asDouble();
+            }
+            Runtime.getRuntime().halt(exitCode);
+            return obj.duplicate();
+        });
+
+        /* @doc mechanism
+         *
+         * @Name freezeThread
+         * @Object ServerTag
+         * @Input ElementTag(Number)
+         * @Description
+         * Forces the current executing thread to sleep (freeze) for the specified number of milliseconds.
+         *
+         * @Warning
+         * SERVER FREEZE / WATCHDOG CRASH!
+         * If executed on the main server thread or a Folia region thread, this will completely
+         * halt all game logic (TPS drops to 0) for the duration. If the duration is too long,
+         * the Server Watchdog will assume the server has crashed and will forcibly terminate it.
+         *
+         * @Usage
+         * // Freezes the current thread for 5 seconds (5000 ms)
+         * - adjust <server> freezeThread:5000
+         */
+        MECHANISM_PROCESSOR.registerMechanism("freezeThread", (obj, val) -> {
+            if (val instanceof ElementTag el && el.isDouble()) {
+                long ms = (long) el.asDouble();
+                try {
+                    Thread.sleep(ms);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            return obj.duplicate();
+        });
+
+        /* @doc mechanism
+         *
+         * @Name forceCrash
+         * @Object ServerTag
+         * @Input ElementTag (String message)
+         * @Description
+         * Throws a fatal java.lang.Error on the current thread, intentionally simulating a catastrophic JVM failure.
+         *
+         * @Warning
+         * THIS WILL CRASH THE SERVER OR THREAD!
+         * This skips standard exception handling and throws an Error, which will likely
+         * trigger an emergency shutdown or crash report depending on the server software.
+         *
+         * @Usage
+         * - adjust <server> forceCrash:"Intentional script crash"
+         */
+        MECHANISM_PROCESSOR.registerMechanism("forceCrash", (obj, val) -> {
+            String reason = val != null ? val.identify() : "Forced crash via CorexVM";
+            throw new Error("Corex Forced JVM Crash: " + reason);
         });
     }
 
@@ -232,18 +336,34 @@ public class ServerTag implements AbstractTag, Flaggable {
     public @NonNull String identify() {
         return PREFIX + "@";
     }
+
     @Override
     public AbstractTag getAttribute(@NonNull Attribute attribute) {
-        return PROCESSOR.process(this, attribute);
+        return TAG_PROCESSOR.process(this, attribute);
     }
 
     @Override
     public @NonNull TagProcessor<ServerTag> getProcessor() {
-        return PROCESSOR;
+        return TAG_PROCESSOR;
     }
 
     @Override
     public @NonNull String getTestValue() {
         return "server@";
+    }
+
+    @Override
+    public @NonNull Adjustable duplicate() {
+        return INSTANCE;
+    }
+
+    @Override
+    public @NonNull AbstractTag applyMechanism(@NonNull String mechanism, @NonNull AbstractTag value) {
+        return MECHANISM_PROCESSOR.process(this, mechanism, value);
+    }
+
+    @Override
+    public @NonNull MechanismProcessor<? extends AbstractTag> getMechanismProcessor() {
+        return MECHANISM_PROCESSOR;
     }
 }
